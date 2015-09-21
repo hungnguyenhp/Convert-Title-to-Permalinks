169f494a4b50a1eeedc01ad6667d804674b51584
Hung Nguyen <info@nguyenanhung.com>
2015-09-22 03:09:49 +0700

    Update file Example.php

--- Example.php
+++ Example.php
@@ -1,6 +1,7 @@
 <?php
 include 'convertV2E.php';
-echo convertV2E('Tôi là Nguyễn An Hưng');	// Toi la Nguyen An Hung
-echo convertV2E('Tôi là Nguyễn An Hưng', true);	// toi la nguyen an hung
-echo convertV2E('Tôi là Nguyễn An Hưng!', true, 'postname');	// toi la nguyen an hung
-echo convertV2E('Tôi là Nguyễn An Hưng!', true, 'postname', true);	// toi-la-nguyen-an-hung
+echo convertV2E('Tôi là Nguyễn An Hưng');	// Tra ve gia tri: Toi la Nguyen An Hung
+echo convertV2E('Tôi là Nguyễn An Hưng', true);	// Tra ve gia tri: toi la nguyen an hung
+echo convertV2E('Tôi là Nguyễn An Hưng!', true);	// Tra ve gia tri: toi la nguyen an hung!
+echo convertV2E('Tôi là Nguyễn An Hưng!', true, 'postname');	// Tra ve gia tri: toi la nguyen an hung
+echo convertV2E('Tôi là Nguyễn An Hưng!', true, 'postname', true);	// Tra ve gia tri: toi-la-nguyen-an-hung
